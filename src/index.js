454a28781daf059291dfacc5bcf6dbb9c2098b33
GYT <349915792@qq.com>
2020-05-15 03:35:59 +0800

    开起serviceWorker 的时候在build下面的service-workder 添加 fetch  暂时还不知道 怎么在config-overrides 里面配置addEventListenner('fetch')

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ConfigProvider } from 'antd'
 import store from './store'
 import zhCN from 'antd/es/locale/zh_CN';
 import 'animate.css'
-// import { isDev } from 'utils/config'
+import { isDev } from 'utils/config'
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -28,4 +28,33 @@ ReactDOM.render(<Provider store={store}>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (isDev) {
+  serviceWorker.unregister();
+} else {
+  serviceWorker.register();
+
+  //   let BUCKET_CURRENT = 'cache-v2'
+  // // 请求拦截
+  // self.addEventListener('fetch', function (event) {
+  //   console.log('[EVENT_FETCH]', event)
+  //   event.respondWith(
+  //     caches.match(event.request).then(function (resp) {
+  //       // console.log('[REQUEST]:', event.request)
+  //       var reqToCache = event.request.clone()
+
+  //       return resp || fetch(event.request).then(function (response) {
+  //         console.log('[EVENT_FETCH] lose cache')
+  //         var respToCache = response.clone();
+  //         // console.log('[RESPONSE]:', response)
+  //         caches.open(BUCKET_CURRENT).then(function (cache) {
+  //           cache.put(reqToCache, respToCache);
+  //         });
+  //         return response
+  //       });
+  //     }).catch(function (e) {
+  //       console.log('[EVENT_FETCH] fetch error', e)
+  //     })
+  //   );
+  // });
+  // // ===============
+}
