d1cccdc80970e7881f9c77090fb2c45c11b44ea3
GYT <349915792@qq.com>
2020-06-14 14:07:47 +0800

    push default

--- git.sh
+++ git.sh
@@ -1,27 +1,54 @@
 #!/bin/bash
 
-msg=$1
 
-if [ ! -n "$msg" ]; then  
-    msg="."
+function gitMerge () {
+    # git checkout master
+    git checkout master
+    echo '-------- git has checkout branch master -----'
+    # git pull
+    git pull 
+    echo '-------- git has pull   ------------'
+    # git checkout dev
+    echo '-------- git chekout dev -----------'
+    git chekout dev
+    # git merge from master
+    git merge master
+    echo '------- git merge master  ----------'
+}
+
+function gitPush() {
+    # add
+
+    git add -A
+
+    echo '---------- (git add) end ----------'
+
+    # commit
+
+    git commit -m "$msg"
+
+    echo "---------- (git commit) end ----------"
+
+    # push
+
+    git push 
+
+    echo '---------- (push origin master) end ----------'
+
+    echo 'over.'
+}
+
+
+
+msg=$1
+if [ ! -n "$msg" ]
+then
+    msg='push default'
 fi
 
-# add
-
-git add -A
-
-echo '---------- (git add) end ----------'
-
-# commit
-
-git commit -m "$msg"
-
-echo "---------- (git commit) end ----------"
-
-# push
-
-git push 
-
-echo '---------- (push origin master) end ----------'
-
-echo 'over.'
+if [ $msg -eq 'merge' ]
+then
+    echo '------$msg'
+else 
+    gitPush
+fi
