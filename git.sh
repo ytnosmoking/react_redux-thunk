ba67c4c9d63b18e6b747e48d159f2a5991b07165
GYT <349915792@qq.com>
2020-06-14 14:31:26 +0800

    mergeTo

--- git.sh
+++ git.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+function gitPush() {
+    # add
+    git add -A
+    echo '---------- (git add) end ----------'
 
-function gitMerge () {
+    # commit
+    git commit -m "$msg"
+    echo "---------- (git commit) end ----------"
+
+    # push
+    git push 
+    echo '---------- (push origin master) end ----------'
+    echo 'over. push'
+}
+
+function gitMergeFrom () {
+    gitPush
+    echo "---------- git merger from  --------------"
     # git checkout master
     git checkout master
     echo '-------- git has checkout branch master -----'
@@ -16,32 +32,29 @@ function gitMerge () {
     echo '------- git merge master  ----------'
 }
 
-function gitPush() {
-    # add
+function gitMergeTo () {
 
-    git add -A
+    gitPush
+    echo "---------- git merger from  --------------"
 
-    echo '---------- (git add) end ----------'
-
-    # commit
-
-    git commit -m "$msg"
-
-    echo "---------- (git commit) end ----------"
-
-    # push
-
-    git push 
-
-    echo '---------- (push origin master) end ----------'
-
-    echo 'over.'
+     # git pull
+    git pull 
+    echo '-------- git has pull   ------------'
+    # git checkout master
+    echo '-------- git chekout master -----------'
+    git chekout master
+    # git merge from master
+    git merge dev
+    echo '------- git merge dev  ----------'
 }
+
+
 
 
 
 msg="$1"
 merge="merge"
+mergeTo="mergeTo"
 
 if [ ! -n "$msg" ]
 then
@@ -50,7 +63,12 @@ fi
 
 if [ "$msg" == "$merge" ]
 then
-    echo '------$msg'
+    echo "------ will merge from "
+
+elif [ "$msg" == "$mergeTo"]
+then 
+    echo "------ will merge to "
+    
 else 
     gitPush
 fi
